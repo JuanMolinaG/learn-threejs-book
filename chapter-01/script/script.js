3ff378ee042e6bfbf02ec3bd3c12fe1200023f95
Juan G. Molina <jg.molina92@hotmail.com>
2024-03-11 18:10:32 -0500

    Chapter 1: add lights

--- chapter-01/script/script.js
+++ chapter-01/script/script.js
@@ -12,41 +12,51 @@ const init = () => {
   const renderer = new THREE.WebGLRenderer()
   renderer.setClearColor(new THREE.Color(0x000000))
   renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.shadowMap.enabled = true
 
-  const axes = new THREE.AxesHelper(20)
-  scene.add(axes)
+  // const axes = new THREE.AxesHelper(20)
+  // scene.add(axes)
 
   const planeGeometry = new THREE.PlaneGeometry(60, 20)
-  const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa })
-
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xaaaaaa })
   const plane = new THREE.Mesh(planeGeometry, planeMaterial)
   plane.rotation.x = -0.5 * Math.PI
   plane.position.set(15, 0, 0)
+  plane.receiveShadow = true
   scene.add(plane)
 
   // create a cube
   const cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
-  const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-    wireframe: true
+  const cubeMaterial = new THREE.MeshLambertMaterial({
+    color: 0xff0000
   })
   const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
   cube.position.set(-4, 3, 0)
+  cube.castShadow = true
   scene.add(cube)
 
   // create a sphere
   const sphereGeometry = new THREE.SphereGeometry(4, 20, 20)
-  const sphereMaterial = new THREE.MeshBasicMaterial({
-    color: 0x7777ff,
-    wireframe: true
+  const sphereMaterial = new THREE.MeshLambertMaterial({
+    color: 0x7777ff
   })
   const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
   sphere.position.set(20, 4, 2)
+  sphere.castShadow = true
   scene.add(sphere)
 
   // position and point the camera to the center of the scene
   camera.position.set(-30, 40, 30)
   camera.lookAt(scene.position)
+
+  // add lights
+  const spotLight = new THREE.SpotLight(0xffffff, 8000)
+  spotLight.position.set(-40, 40, -15)
+  spotLight.castShadow = true
+  spotLight.shadow.mapSize = new THREE.Vector2(4096, 4096)
+  spotLight.shadow.camera.far = 130
+  spotLight.shadow.camera.near = 40
+  scene.add(spotLight)
 
   // add the output of the renderer to the html element
   document.body.appendChild(renderer.domElement)
